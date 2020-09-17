d10a154939bd7e58adb730b50b714b913f3457fe
JonShard <jonemskaara@gmail.com>
2020-09-17 16:31:13 +0200

    Add MapAnalyser skip transition if time between the two notes is big enough

--- src/MapAnalyzer.cpp
+++ src/MapAnalyzer.cpp
@@ -6,9 +6,12 @@ TransitionMatrix<bool> MapAnalyzer::AnalyzeMap(Map map) {
     TransitionMatrix<bool> matrix;
 
     for (int i = 0; i < map.m_notes.size()-1; i++) {
-        //TODO: for all notes on same frame. Util func
         Note n = map.m_notes[i];
         Note nn = map.m_notes[i+1];
+        // If two notes are far enough apart, it's not a transition because the player gets time to "reset" their pose.
+        if (nn.time - n.time > Config::generator.validator.validateTimeAfterNote) {
+            continue;
+        }
         matrix.m_matrix[nn.type][n.type][nn.cutDirection][n.cutDirection][nn.lineLayer][n.lineLayer][nn.lineIndex][n.lineIndex] = true;
     }
 
