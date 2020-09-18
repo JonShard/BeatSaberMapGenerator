2b09d249383ad2c2551cab6755b46ce50b707e05
JonShard <jonemskaara@gmail.com>
2020-09-18 23:22:02 +0200

    Fix isFileExtention function looking for first '.' instead of the last

--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -8,7 +8,7 @@ int rng(int min, int max) {
 }
 
 bool isFileExtention(const std::string file, const std::string ext) {
-    int pos = file.find(".");
+    int pos = file.find_last_of(".");
     if (pos < 0) 
         return false;
     std::string splitExt = file.substr(pos, file.length());
