f20ae60332385aaef1b88d9307e4a4b4f8ef5391
JonShard <jonemskaara@gmail.com>
2020-10-06 20:20:03 +0200

    Fix song export script not selecting .dat files correctly

--- scripts/export.sh
+++ scripts/export.sh
@@ -8,16 +8,16 @@ cd .temp
 for src in ../songs/* ; do
 
     dest="${src##*/}"
-    echo "$dest"
     mkdir -p "$dest"
     cp -r "$src" .
     for m in "$dest"/*.ogg; do
         mv -- "$m" "${m%.ogg}.egg" &>/dev/null
     done
     for d in "$dest"/*.dat; do
-        if [ "$d" = "info.dat" ] || [ "$d" = "Info.dat" ] || [ "$d" = "song.dat" ]; then
+        if [[ "$d" == *"info.dat" ]] || [[ "$d" == *"Info.dat" ]] || [[ "$d" == *"song.dat" ]]; then
             continue
         fi
+        printf "$dest/song.dat -> $d\n"
         mv "$dest"/song.dat "$d" &>/dev/null
     done
     cd "$dest"
@@ -27,3 +27,4 @@ done
 
 cd ..
 rm -r .temp
+
