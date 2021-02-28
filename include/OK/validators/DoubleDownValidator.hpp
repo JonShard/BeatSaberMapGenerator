894cc069bcef61547a9f511d3527041b13e64958
JonShard <jonemskaara@gmail.com>
2021-02-28 10:23:58 +0100

    Refactor DoubleDownValidator to use Cluster

--- include/OK/validators/DoubleDownValidator.hpp
+++ include/OK/validators/DoubleDownValidator.hpp
@@ -19,26 +19,27 @@ public:
 
 
     virtual bool validate(Map map) {
-        for (int i = 1; i < map.m_notes.size(); i++) {
+        std::vector<Note> notes = map.getNotes();
+        for (int i = 1; i < notes.size(); i++) {
             for (int j = i - 1; j >= 0; j--) {
-                if (std::abs(map.m_notes[i].m_time - map.m_notes[j].m_time) < Config::generator.noteClusterTime) // Skip the ones in the same cluster.
+                if (std::abs(notes[i].m_time - notes[j].m_time) < Config::generator.noteClusterTime) // Skip the ones in the same cluster.
                     continue;
 
-                if (map.m_notes[i].m_type != map.m_notes[j].m_type) // Has to be same color to be double down.
+                if (notes[i].m_type != notes[j].m_type) // Has to be same color to be double down.
                     continue;
 
-                int angleDelta = Util::angleDelta(CutAngle[map.m_notes[i].m_cutDirection], CutAngle[map.m_notes[j].m_cutDirection]);
+                int angleDelta = Util::angleDelta(CutAngle[notes[i].m_cutDirection], CutAngle[notes[j].m_cutDirection]);
 
                 if (angleDelta <= Config::generator.validator.doubleDown.angleToBeDoubleDown) {
                     m_fails++;
                     Validator::s_totalFails++;
-                    return false;                    
+                    return false;
                 }
                 else {  // Last note wasn't double down, time to stop looking so we don't compare with notes that aren't the previous one.
                     break;
                 }
-
-                if (map.m_notes[i].m_time - map.m_notes[j].m_time > Config::generator.validator.validateTimeAfterNote) {
+                // Stop looking if there is enough space in between the notes.
+                if (notes[i].m_time - notes[j].m_time > Config::generator.validator.validateTimeAfterNote) {
                     break;
                 }
             }
